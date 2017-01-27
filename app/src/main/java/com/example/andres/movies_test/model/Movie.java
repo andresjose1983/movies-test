d3ec310d5e0d213e9cfaaa36dd222fd84fed09ed
andresjose1983 <andresjose1983@gmail.com>
2017-01-27 08:54:59 -0400

    Created Movie model

---
+++ app/src/main/java/com/example/andres/movies_test/model/Movie.java
@@ -0,0 +1,139 @@
+package com.example.andres.movies_test.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by andres on 27/01/17.
+ */
+
+public class Movie implements Serializable {
+
+    private int id;
+
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+
+    private Genre genre;
+
+    @SerializedName("original_language")
+    private String originalLanguage;
+
+    @SerializedName("original_title")
+    private String originalTitle;
+
+    private String overview;
+
+    @SerializedName("release_date")
+    private String date;
+
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    private String title;
+
+    @SerializedName("vote_average")
+    private double vote;
+
+    public Movie(int id, String backdropPath, String originalLanguage, String originalTitle,
+                 String overview, String date, String posterPath, String title, double vote) {
+        this.id = id;
+        this.backdropPath = backdropPath;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.date = date;
+        this.posterPath = posterPath;
+        this.title = title;
+        this.vote = vote;
+    }
+
+    /**
+     * Set movie genre
+     * @param genre
+     */
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * Get id
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Get back image
+     * @return
+     */
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    /**
+     * Get genre
+     * @return
+     */
+    public Genre getGenre() {
+        return genre;
+    }
+
+    /**
+     * Get original lang
+     * @return
+     */
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    /**
+     * Get original title
+     * @return
+     */
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    /**
+     * Get content
+     * @return
+     */
+    public String getOverview() {
+        return overview;
+    }
+
+    /**
+     * Get date
+     * @return
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * Get poster image
+     * @return
+     */
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    /**
+     * Get title
+     * @return
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Get movie vote
+     * @return
+     */
+    public double getVote() {
+        return vote;
+    }
+}
